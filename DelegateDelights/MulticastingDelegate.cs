3f10987054cca78081642cc48bcd18fc43435a14
Kilisio Veronicah <kilisionicah@gmail.com>
2023-05-31 06:01:08 +0300

    First commit

---
+++ DelegateDelights/MulticastingDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+delegate int NumberChanger2(int n);
+namespace DelegateAppl
+{
+    class MulticastingDelegate
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
+    }
+}
---
+++ DelegateDelights/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateDelights.PrintString;
+
+delegate int NumberChanger(int n);
+
+namespace DelegateDelights
+{
+    public class program
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+            NumberChanger nc;
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+
+
+            //Multicasting 
+            nc = nc1;
+            nc += nc2;
+
+            //calling multicast
+            nc(5);
+            Console.WriteLine("Value of Num after Multicast: {0}", getNum());
+            Console.ReadKey();
+
+
+
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            sendString(ps1);
+            sendString(ps2);
+            Console.ReadKey();
+        }
+    }
+}
